d665df708c870e56da71edc227bd71c43b1db54d
xiangzhoumeng <xiangzhoumeng@kugou.net>
2016-08-31 19:16:20 +0800

    菜单修改

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,6 +10,7 @@ export default class NavBar extends Component {
     const tap2 = require('../../../static/assets/ic_tap2.png');
     const tap3 = require('../../../static/assets/ic_tap3.png');
 
+    console.log(this)
     const styles = require('./NavBar.scss');
     return (
       <ul className={styles.navBar}>
@@ -17,7 +18,10 @@ export default class NavBar extends Component {
           list && list.map((item,index) =>
             <li key={'nav-bar'+index}>
               <Link to={'/'+item.link} className={styles.nav} activeClassName={styles.active}>
-                <img src={index == 1 ? tap2 : tap3} className={styles.tap}/>{item.name}
+                {index == 0 && <em className={styles.tap + ' '+styles.tap0 + ' f-ib'}></em>}
+                {index == 1 && <em className={styles.tap + ' '+styles.tap1 + ' f-ib'}></em>}
+                {index == 2 && <em className={styles.tap + ' '+styles.tap2 + ' f-ib'}></em>}
+                {item.name}
               </Link>
             </li>
           )
