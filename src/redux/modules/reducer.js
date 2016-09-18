d8d7b3161d8d01151d04c92353e6e59530588c16
xiangzhoumeng <xiangzhoumeng@kugou.net>
2016-09-18 16:58:43 +0800

    清理reducer

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -2,12 +2,6 @@ import { combineReducers } from 'redux';
 import multireducer from 'multireducer';
 import { routerReducer } from 'react-router-redux';
 import {reducer as reduxAsyncConnect} from 'redux-async-connect';
-
-import auth from './auth';
-import counter from './counter';
-import {reducer as form} from 'redux-form';
-import info from './info';
-import widgets from './widgets';
 import navlist from './navlist';
 import home from './home';
 import loading from './loading';
@@ -33,15 +27,6 @@ import history from './history';
 export default combineReducers({
   routing: routerReducer,
   reduxAsyncConnect,
-  auth,
-  form,
-  multireducer: multireducer({
-    counter1: counter,
-    counter2: counter,
-    counter3: counter
-  }),
-  info,
-  widgets,
   navlist,
   home,
   loading,
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {IndexRoute, Route} from 'react-router';
-import { isLoaded as isAuthLoaded, load as loadAuth } from 'redux/modules/auth';
 import {
   App,
   Home,
@@ -32,20 +31,20 @@ import {
 } from './containers/Mine';
 export default (store) => {
   const requireLogin = (nextState, replace, cb) => {
-    function checkAuth() {
-      const { auth: { user }} = store.getState();
-      if (!user) {
-        // oops, not logged in, so can't be here!
-        replace('/');
-      }
-      cb();
-    }
-
-    if (!isAuthLoaded(store.getState())) {
-      store.dispatch(loadAuth()).then(checkAuth);
-    } else {
-      checkAuth();
-    }
+    //function checkAuth() {
+    //  const { auth: { user }} = store.getState();
+    //  if (!user) {
+    //    // oops, not logged in, so can't be here!
+    //    replace('/');
+    //  }
+    //  cb();
+    //}
+    //
+    //if (!isAuthLoaded(store.getState())) {
+    //  store.dispatch(loadAuth()).then(checkAuth);
+    //} else {
+    //  checkAuth();
+    //}
   };
 
   /**
